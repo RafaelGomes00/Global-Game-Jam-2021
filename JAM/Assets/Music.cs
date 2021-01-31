503a53907e76c4db34af5d0a2e3eafb2e6faa8bd
Guilherme Torres <guilhermetorres97@gmail.com>
2021-01-31 07:38:18 -0800

    musica

---
+++ JAM/Assets/Music.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    [SerializeField] private bool playOnStart;
+    [SerializeField] private AudioSource audio;
+    [SerializeField] private float loopStart;
+    [SerializeField] private float loopEnd;
+
+    public bool shouldReplay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shouldReplay = false;
+        if (playOnStart)
+        {
+            audio.Play();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (audio.time > loopStart)
+        {
+            shouldReplay = true;
+        }
+        if (!audio.isPlaying && shouldReplay)
+        {
+            audio.time = loopStart;
+            audio.Play();
+        }
+    }
+}
